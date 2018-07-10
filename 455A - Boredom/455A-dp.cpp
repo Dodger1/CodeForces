d2cc8d8aca41774ce8118e7b8a8b48015d0b93f4
Dodger1 <40784909+Dodger1@users.noreply.github.com>
2018-07-10 23:01:00 +0200

    Update 455A-dp.cpp
    you don't need to go all the way long to 100001,  you need to just determine the biggest number and add 2 to it in the loop and you will have the same result

--- 455A - Boredom/455A-dp.cpp
+++ 455A - Boredom/455A-dp.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	const int MAX_N = 100001;
 
-	int n, x;
+	int n, x , max1 = 0;
 	vector<bigInt> dp(MAX_N, 0);
 	vector<bigInt>  c(MAX_N, 0);
 
@@ -19,14 +19,16 @@ int main()
 	while(n--) {
 		cin >> x;
 		c[x]++;
+		if(x>max1)
+			max1 = x ;
 	}
 
 	dp[0] = 0;
 	dp[1] = c[1];
 
-	for (int i = 2; i < MAX_N; i++) {
+	for (int i = 2; i < max1 + 2; i++) {
 		dp[i] = max(dp[i - 1], dp[i - 2] + i*c[i]);
 	}
 
-	cout << dp[MAX_N - 1];
+	cout << dp[max1 + 2 - 1];
 }
